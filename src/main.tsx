2aab107ce7eb2a446bde8b30f309b8b7f21ca82e
samy boutalbi <s.boutalbi@esi-sba.dz>
2025-01-31 16:59:38 +0100

    update sign up page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
+import SignUp from "./modules/features/auth/components/SignUP/signUp";
 
 const root = document.getElementById("root");
 
@@ -10,8 +11,9 @@ if (root) {
     <StrictMode>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />}>
-          </Route>            
+          <Route path="/" element={<App />} />
+          <Route path="/signup" element={<SignUp />}></Route>
+          
         </Routes>
       </BrowserRouter>
     </StrictMode>
