2d9e7fad7aff3c6debca34f0a93f926711e999f2
zaki <z.goumri@esi-sba.dz>
2025-02-02 09:59:37 +0100

    before change tailwind version 0.1

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import App from "./App";
-// import SignUp from "./modules/features/auth/components/SignUP/signUp";
-import SignIn from "./modules/features/auth/components/SignIN/signIn";
 
 const root = document.getElementById("root");
 
@@ -13,7 +11,6 @@ if (root) {
       <Router>
         <Routes>
           <Route path="/" element={<App />} />
-          <Route path="/signup" element={<SignIn />}/>
         </Routes>
       </Router>
     </StrictMode>
