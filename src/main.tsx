0d237a53b6013966612fb011a5cb6ddbb8953570
Mouloud Hasrane <m.hasrane@esi-sba.dz>
2025-02-02 21:03:48 +0100

    Created forgot password page

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
 import "./index.css"
 import React, { Suspense } from 'react';
+import { ForgotPassword } from "./modules/features/auth/forgot-password/page";
 const App = React.lazy(() => import('./App'));
 const Signin = React.lazy(() => import('./modules/features/auth/signin/page'));
 const Footer = React.lazy(() => import('./modules/shared/components/footer'));
@@ -17,6 +18,7 @@ if (root) {
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/signin" element={<Signin/>} />
+          <Route path='/forgot-password' element={<ForgotPassword/>} />
         </Routes>
       </Router>
       </Suspense>
