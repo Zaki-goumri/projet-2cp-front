04b86461fb61932ca27a2d2d17bdcf4278384e5d
Mouloud Hasrane <m.hasrane@esi-sba.dz>
2025-02-03 01:33:19 +0100

    finished ui forgot-passPage

--- src/modules/features/auth/forgot-password/page.tsx
+++ src/modules/features/auth/forgot-password/page.tsx
@@ -8,15 +8,17 @@ export const ForgotPassword = (props: {}) => {
   }
 
   return (
-     <main className="flex flex-col justify-around h-[100vh] md:px-10">
-      <div className=' mt-10 flex justify-center '>
+     <main className="flex flex-col justify-around h-[100vh]   md:px-10">
+      <div className=' mt-32 flex justify-center  '>
   <img src="/assets/logo.svg" alt="hero" className="" />
       </div>
       <section className='flex justify-around items-center '>
-        <div className="flex flex-col gap-6 justify-center items-center lg:items-start xl:items-start"><h1 className='text-5xl font-bold flex gap-4 '> Forgot <p className='text-primary text-5xl'> Password</p>?</h1>
+        <div className="flex flex-col   justify-center gap-30 md:gap-6 items-center lg:items-start xl:items-start"><h1 className='text-5xl font-bold flex gap-4 '> Forgot <p className='text-primary text-5xl'> Password</p>?</h1>
+
+          
           
       
-         <h3 className="text-2xl">We've sent you a link to reset your password. Enter your E-mail.</h3>
+         <h3 className="text-2xl md:text-center md:mx-4 sm:text-center text-center lg:text-left lg:mx-0 ">We've sent you a link to reset your password. Enter your E-mail.</h3>
 
           <ForgotPasswordForm submit={onSubmit} />
         </div>
