45559d3fd610d67d1150e11711f692c1fb79d4e5
mouloud240 <m.hasrane@esi-sba.dz>
2025-02-19 19:15:56 +0100

    fixed overlapping QCP with main

--- src/modules/features/auth/forgot-password/page.tsx
+++ src/modules/features/auth/forgot-password/page.tsx
@@ -1,6 +1,5 @@
 import CtaButton from "@/modules/shared/components/CtaButton";
 import {
-  forgotPassformValues,
   ForgotPasswordForm,
 } from "./components/forgotPasswordForm";
 import { Link } from "react-router";
@@ -8,42 +7,43 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
-export const ForgotPassword = (props: {}) => {
-  async function onSubmit(values: forgotPassformValues) {
-    console.log("Reset password link sent to your email" + values.email);
-  }
 
+const ForgotPassword = () => {
   return (
-    <QueryClientProvider client={queryClient}>    <main className="flex flex-col justify-around h-[100vh]   md:px-10">
-      <ToastContainer/>
-      <div className=" mt-32 flex justify-center  ">
-        <Link to={"/"}>
-          {" "}
-          <img src="/assets/logo.svg" alt="hero" className="" />
-        </Link>{" "}
-      </div>
-      <section className="flex justify-around items-center ">
-        <div className="flex flex-col   justify-center gap-30 md:gap-6 items-center lg:items-start xl:items-start">
-          <h1 className="text-5xl font-bold flex gap-4 ">
+    <QueryClientProvider client={queryClient} >
+      <main className="flex flex-col justify-around h-[100vh]   md:px-10">
+        <ToastContainer/>
+        <div className=" mt-32 flex justify-center  ">
+          <Link to={"/"}>
             {" "}
-            Forgot <p className="text-primary text-5xl"> Password</p>?
-          </h1>
-
-          <h3 className="text-2xl md:text-center md:mx-4 sm:text-center text-center lg:text-left lg:mx-0 ">
-            We've sent you a link to reset your password. Enter your E-mail.
-          </h3>
-
-          <ForgotPasswordForm submit={onSubmit} />
+            <img src="/assets/logo.svg" alt="hero" className="" />
+          </Link>{" "}
         </div>
+        <section className="flex justify-around items-center ">
+          <div className="flex flex-col   justify-center gap-30 md:gap-6 items-center lg:items-start xl:items-start">
+            <h1 className="text-5xl font-bold flex gap-4 ">
+              {" "}
+              Forgot <p className="text-primary text-5xl"> Password</p>?
+            </h1>
 
-        <aside>
-          <img
-            src="/assets/forgotPasswordHero.svg"
-            alt="hero"
-            className="hidden lg:block xl:bloc"
-          />
-        </aside>
-      </section>
-    </main>
-    </QueryClientProvider>  );
+            <h3 className="text-2xl md:text-center md:mx-4 sm:text-center text-center lg:text-left lg:mx-0 ">
+              We've sent you a link to reset your password. Enter your E-mail.
+            </h3>
+
+            <ForgotPasswordForm />
+          </div>
+
+          <aside>
+            <img
+              src="/assets/forgotPasswordHero.svg"
+              alt="hero"
+              className="hidden lg:block xl:bloc"
+            />
+          </aside>
+        </section>
+      </main>
+    </QueryClientProvider>    
+);
+
 };
+export default ForgotPassword;
