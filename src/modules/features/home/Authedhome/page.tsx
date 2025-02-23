a94f7e3c364a0ade306ba4976d350255e8ca4fbd
zaki <z.goumri@esi-sba.dz>
2025-02-23 18:08:41 +0100

    2 sections of authenticated

--- src/modules/features/home/Authedhome/page.tsx
+++ src/modules/features/home/Authedhome/page.tsx
@@ -2,10 +2,20 @@ import React from "react";
 const NavBar = React.lazy(
   () => import("@/modules/features/home/Authedhome/components/navBar"),
 );
+const SearchSection = React.lazy(
+  () => import("@/modules/features/home/Authedhome/components/searchSection"),
+);
+const Oppertunities = React.lazy(
+  () => import("@/modules/features/home/Authedhome/components/oppertunities"),
+);
 
 const AuthedHome = () => {
-  return <main>
+  return (
+    <main  >
   <NavBar />
-  </main>;
-};
+  <SearchSection />
+  <Oppertunities />
+  </main>
+
+  )};
 export default AuthedHome;
