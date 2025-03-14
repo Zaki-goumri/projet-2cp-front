0eb5c2efc7b3b98b8f60cb3a481a25313ac9b9bc
mouloud240 <m.hasrane@esi-sba.dz>
2025-03-14 18:32:34 +0100

    (refactored) forgot password apge

--- src/modules/features/auth/forgot-password/page.tsx
+++ src/modules/features/auth/forgot-password/page.tsx
@@ -1,42 +1,33 @@
 import CtaButton from "@/modules/shared/components/CtaButton";
-import { ForgotPasswordForm } from "./components/forgotPasswordForm";
 import { Link } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ToastContainer } from "react-toastify";
+import React, { lazy } from "react";
 
+const ForgotPasswordForm=lazy(()=>import("@/modules/features/auth/forgot-password/components/forgotPasswordForm"));
 const queryClient = new QueryClient();
 
 const ForgotPassword = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="flex flex-col justify-around h-[100vh]   md:px-10">
+      <main className="flex flex-col justify-around ">
         <ToastContainer />
-        <div className=" mt-32 flex justify-center  ">
-          <Link to={"/"}>
-            <img src="/assets/logo.svg" alt="hero" className="" />
-          </Link>{" "}
-        </div>
-        <section className="flex justify-around items-center ">
-          <div className="flex flex-col   justify-center gap-30 md:gap-6 items-center lg:items-start xl:items-start">
-            <h1 className="text-5xl font-bold flex gap-4 ">
-              Forgot <p className="text-primary text-5xl"> Password</p>?
+          <div className="min-h-screen flex justify-center items-center bg-gradient-to-br from-[#98E9AB]/30 to-[#98E9AB]/50">
+          <div className="bg-white p-8 rounded-xl shadow-lg mx-4 w-full max-w-md flex justify-evenly gap-10 items-center flex-col">
+            <img src="/assets/logo.svg" alt="Logo" className="h-8" />
+            <div className="flex flex-col gap-2 justify-center items-center">
+              <h1 className="text-3xl font-bold ">
+              Forgot Password
             </h1>
-            <h3 className="text-2xl md:text-center md:mx-4 sm:text-center text-center lg:text-left lg:mx-0 ">
-              We've sent you a link to reset your password. Enter your E-mail.
-            </h3>
+              <div className="text-center text-lg text-gray-500">
+                Type Your Email Address To change the password
+              </div>
+            </div>            <ForgotPasswordForm/>
 
-            <ForgotPasswordForm />
           </div>
+     </div>
 
-          <aside>
-            <img
-              src="/assets/forgotPasswordHero.svg"
-              alt="hero"
-              className="hidden lg:block xl:bloc"
-            />
-          </aside>
-        </section>
-      </main>
+              </main>
     </QueryClientProvider>
   );
 };
