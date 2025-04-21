6a141bf8f6fa16b0633a072b177cc1be991c929f
zaki <z.goumri@esi-sba.dz>
2025-04-21 13:03:52 +0100

    pulling

--- src/modules/teams/pages/CreateTeamPage.tsx
+++ src/modules/teams/pages/CreateTeamPage.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import CreateTeamCard from '../components/CreateTeamCard';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-// Initialize QueryClient
-const queryClient = new QueryClient();
 
 const CreateTeamPage: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-gray-50 py-10">
-        <div className="container mx-auto">
-          <CreateTeamCard />
-        </div>
+    <div className="min-h-screen bg-gray-50 py-10">
+      <div className="container mx-auto">
+        <CreateTeamCard />
       </div>
-    </QueryClientProvider>
+    </div>
   );
 };
 
